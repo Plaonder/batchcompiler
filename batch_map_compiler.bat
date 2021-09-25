eca95a71d293cbd529c0592e87cae5a284912e95
Plaonder Joander <biz@jayden.phindog.family>
2021-09-24 23:21:50 -0700

    Actually able to set map directory :eyes:

--- batch_map_compiler.bat
+++ batch_map_compiler.bat
@@ -41,19 +41,12 @@ goto gamequestions
 :: Steam directory check
 for /f "usebackq tokens=1,2,*" %%i in (`reg query "HKCU\Software\Valve\Steam" /v "SteamPath"`) do set "steampath=%%~k"
 set steampath=%steampath:/=\%
-if not exist "%steampath%\steam.exe" (
-	if not exist "%ProgramFiles(x86)%\steam\steam.exe" (
-		if not exist "%ProgramFiles%\steam\steam.exe" (
-			echo Couldn't find the steam directory! Do you have it installed?
-		) else (
-			set "steampath=%ProgramFiles%\steam"
-		)
-	) else set "steampath=%ProgramFiles(x86)%\steam"
-)
+if not exist "%steampath%\steam.exe" (echo Couldn't find the steam directory! Do you have it installed?)
 
 :: Check if the steam directory has Half-Life: Alyx to make life easier.
 if exist "%steampath%\steamapps\common\Half-Life Alyx\" (
-	SET "hla_dir=%steampath%\steamapps\common\"Half-Life Alyx"\"
+    echo Found Half-Life Alyx inside of your Steam directory! Continuing..
+	set "hla_dir=%steampath%\steamapps\common\Half-Life Alyx\"
 ) else (
     :: Uh oh! Didn't find it in the directory, manually have them put in the directory.
     echo.
@@ -61,16 +54,22 @@ if exist "%steampath%\steamapps\common\Half-Life Alyx\" (
     echo Please enter your Half-Life: Alyx directory here.
     echo.
     set /P "hla_dir="
-    if not exist "%hla_dir%" (
-        echo.
-        echo Directory does not exist, please try again.
-        goto compilehla
-    )
+)
+echo Directory exists! Continuing...
+echo.
+
+echo Please enter the directory to the maps folder that you want to compile.
+set /P map_dir=
+
+if not exist "%map_dir%" (
+    echo That directory does not exist.. Restarting.
+    goto compilehla
 )
 
-for %%i in ("O:\Games\SteamLibrary\steamapps\common\Half-Life Alyx\content\hlvr_addons\industrial_discharge_2\maps\*.vmap") do (
+
+for %%i in ("%map_dir%\*.vmap") do (
     echo Starting compile..
-    start /WAIT call compile_map.bat 2048 hla "%%i"
+    start /WAIT call compile_map.bat 512 hla "%%i" "%hla_dir%"
     echo Done! Moving to the next map.
     echo.
 )
